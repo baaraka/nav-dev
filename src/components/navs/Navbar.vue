f5ca189986a1a945b4c70c3ffd6b34e0af729ae4
barakaganai <barakaganai01@gmail.com>
2024-08-07 09:20:46 +0300

    create navbar.vue

---
+++ src/components/navs/Navbar.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <div
+    class="flex p-5 container items-center justify-between bg-[#222121] shadow-md text-white h-20"
+  >
+    <router-link to="/">
+      <h1 class="text-3xl font-bold">Navigation</h1>
+    </router-link>
+
+    <div class="flex items-center gap-5">
+      <router-link to="/customer"
+        ><span class="text-xl">Customer</span></router-link
+      >
+      <router-link to="/rider"><span class="text-xl">Rider</span></router-link>
+    </div>
+  </div>
+</template>
