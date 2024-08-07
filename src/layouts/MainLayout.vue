15553dc634ec4ac84dd06b119a637a86eacfec91
barakaganai <barakaganai01@gmail.com>
2024-08-07 09:20:18 +0300

    create mainLayout.vue

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Navbar from "@/components/navs/Navbar.vue";
+</script>
+
+<template>
+  <div class="h-screen">
+    <Navbar />
+    <main class="flex-grow">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
