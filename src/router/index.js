f68999ac6941c3e6faa9b18653bd1a181564217b
barakaganai <barakaganai01@gmail.com>
2024-08-07 09:19:53 +0300

    create index route

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from "vue-router";
+import MainLayout from "@/layouts/MainLayout.vue";
+import HomePage from "@/pages/HomePage.vue";
+import CustomerPage from "@/pages/CustomerPage.vue";
+import RiderPage from "@/pages/RiderPage.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "mainLayout",
+      component: MainLayout,
+      children: [
+        {
+          path: "",
+          name: "home",
+          component: HomePage,
+        },
+        {
+          path: "/customer",
+          name: "customer",
+          component: CustomerPage,
+        },
+        {
+          path: "/rider",
+          name: "rider",
+          component: RiderPage,
+        },
+      ],
+    },
+  ],
+});
+
+export default router;
